b6b162a4ac630068ecada1cb9754dc2b325c0680
Jesse McReady <jessemcready@gmail.com>
2018-06-28 20:18:36 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -53,7 +53,7 @@ def get_average_age_for_season(data, season)
   data.each do |season_number, contestants|
     if season_number == season
       contestants.each do |person|
-        ages << person["age"].to_i  
+        ages << person["age"].to_f  
       end
     end
   end
